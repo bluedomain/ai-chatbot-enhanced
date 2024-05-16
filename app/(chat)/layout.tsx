dd01778cfc31382ca81cb4ca23c02d2b92c3ab7b
bluedomain <bluedomainsky@gmail.com>
2024-05-15 20:07:42 -0700

    added analytics

--- app/(chat)/layout.tsx
+++ app/(chat)/layout.tsx
@@ -1,4 +1,5 @@
 import { SidebarDesktop } from '@/components/sidebar-desktop'
+import { Analytics } from "@vercel/analytics/react"
 
 interface ChatLayoutProps {
   children: React.ReactNode
@@ -7,6 +8,7 @@ interface ChatLayoutProps {
 export default async function ChatLayout({ children }: ChatLayoutProps) {
   return (
     <div className="relative flex h-[calc(100vh_-_theme(spacing.16))] overflow-hidden">
+      <Analytics />
       <SidebarDesktop />
       {children}
     </div>
